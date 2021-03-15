31c6caa70579535b6a6d2e46f403c9340e975836
Luis Ramos <luis.ramos@hey.com>
2021-03-15 18:58:46 +1100

    Add checkPreCI task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.konan.properties.loadProperties
+
 buildscript {
     repositories {
         gradlePluginPortal()
@@ -16,5 +18,23 @@ allprojects {
         google()
         mavenCentral()
         jcenter()
+    }
+}
+
+tasks.register("checkPreCI") {
+    description = "Check pre CI config"
+
+    val keystorePropsFile = File("keystore.properties")
+    if (keystorePropsFile.exists()) {
+        println("> keystore.properties found")
+        val keystore = loadProperties("keystore.properties")
+        val signingFile = File(keystore["storeFile"].toString())
+        if (signingFile.exists()) {
+            println("> signing file found")
+        } else {
+            println("Error: signing file not found")
+        }
+    } else {
+        println("Error: keystore.properties not found")
     }
 }
